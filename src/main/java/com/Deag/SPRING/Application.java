b81b01153ad30014e1e1ac47ba4cebe5dbb58bb6
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-09-13 16:04:48 +0000

    helloo world

--- src/main/java/com/Deag/SPRING/Application.java
+++ src/main/java/com/Deag/SPRING/Application.java
@@ -1,12 +1,29 @@
 package com.Deag.SPRING;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@EnableAutoConfiguration
+@Controller
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+
+	public static void main(String[] args)
+	{
+
+
+
 		SpringApplication.run(Application.class, args);
 	}
 }
